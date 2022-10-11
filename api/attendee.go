0feb353d1a83e62f5cfe0972a02e11e0ead83667
AntonyIS <antonyshikubu@gmail.com>
2022-10-11 23:14:34 +0300

    Adding routes for attendees

--- api/attendee.go
+++ api/attendee.go
@@ -1,7 +1,99 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-joint/app"
+	"net/http"
 
-func (h *AttendeeHandler) Get(c *gin.Context){
-	// test
+	"github.com/gin-gonic/gin"
+)
+
+type attendeeHandler struct {
+	attendeeService app.AttendeeService
+}
+
+func NewAttendeeHandler(attendeeService app.AttendeeService) RouteHandler {
+	return attendeeHandler{
+		attendeeService,
+	}
+}
+
+func (h attendeeHandler) Post(c *gin.Context) {
+	var attendee app.Attendee
+
+	if err := c.ShouldBindJSON(&attendee); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	_, err := h.attendeeService.Create(&attendee)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"attendee": attendee,
+	})
+
+}
+
+func (h attendeeHandler) Get(c *gin.Context) {
+	id := c.Param("id")
+	attendee, err := h.attendeeService.Read(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"attendee": attendee})
+}
+
+func (h attendeeHandler) GetAll(c *gin.Context) {
+	attendees, err := h.attendeeService.ReadAll()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"attendees": attendees})
+}
+
+func (h attendeeHandler) Put(c *gin.Context) {
+	var attendee app.Attendee
+
+	if err := c.ShouldBindJSON(&attendee); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	data, err := h.attendeeService.Update(&attendee)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"attendee": data,
+	})
+}
+
+func (h attendeeHandler) Delete(c *gin.Context) {
+	id := c.Param("id")
+	err := h.attendeeService.Delete(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "attendee deleted successfuly",
+	})
 }
--- api/http.go
+++ api/http.go
@@ -1,24 +1,13 @@
 package api
 
 import (
-	"go-joint/app"
-
 	"github.com/gin-gonic/gin"
 )
 
 type RouteHandler interface {
 	Get(*gin.Context)
-	POST(*gin.Context)
-	PUT(*gin.Context)
-	DELETE(*gin.Context)
-}
-
-type AttendeeHandler struct {
-	attendeeService app.AttendeeService
-}
-type EventHandler struct {
-	eventService app.EventService
-}
-type TicketHandler struct {
-	ticketService app.TicketService
+	GetAll(*gin.Context)
+	Post(*gin.Context)
+	Put(*gin.Context)
+	Delete(*gin.Context)
 }
